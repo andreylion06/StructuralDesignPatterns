4764c7479dc2e0944c4c2ac22f35f834a67b3451
Andrii Sakhno <andreylion.zt@gmail.com>
2023-04-01 00:26:56 +0300

    Adapter Pattern

---
+++ AdapterPattern/AdapterLibrary/FileLoggerAdapter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdapterPattern.AdapterLibrary
+{
+    public class FileLoggerAdapter : Logger
+    {
+       private FileWriter _fileWriter;
+
+        public FileLoggerAdapter(FileWriter fileWriter)
+        {
+            _fileWriter = fileWriter;
+        }
+
+        public override void Log(string message)
+        {
+            _fileWriter.WriteLine($"Log: {message}");
+        }
+
+        public override void Error(string message)
+        {
+            _fileWriter.WriteLine($"Error: {message}");
+        }
+
+        public override void Warn(string message)
+        {
+            _fileWriter.WriteLine($"Warn: {message}");
+        }
+    }
+}
